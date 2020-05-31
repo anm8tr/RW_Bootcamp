00905a3cd2f28732ecf4e74beacec74e340e6858
Richard Garrison <threeduser@hotmail.com>
2020-05-30 23:18:21 -0700

    Add new project

--- Week1/ColorPickerChallenge/ColorPickerChallenge/ViewController.swift
+++ Week1/ColorPickerChallenge/ColorPickerChallenge/ViewController.swift
@@ -14,36 +14,40 @@ class ViewController: UIViewController {
     @IBOutlet weak var greenSlider: UISlider!
     @IBOutlet weak var blueSlider: UISlider!
     
+    //@IBOutlet weak var colorView: UIView!
     
-    @IBOutlet weak var rdLabelValue: UILabel!
-    @IBOutlet weak var grLabelValue: UILabel!
-    @IBOutlet weak var blLabelValue: UILabel!
-    //@IBOutlet var colorViewController: UIView!
+    @IBOutlet weak var redLabelValue: UILabel!
+    @IBOutlet weak var greenLabelValue: UILabel!
+    @IBOutlet weak var blueLabelValue: UILabel!
     
-    var red: Float = 0
-    var green: Float = 0
-    var blue: Float = 0
+    var red : Float = 0
+    var green : Float = 0
+    var blue : Float = 0
     
-    let roundedValue = {
-        
-    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
         redSlider.tag = 1
         greenSlider.tag = 2
         blueSlider.tag = 3
         
-        rdLabelValue.tag = 1
-        grLabelValue.tag = 2
-        blLabelValue.tag = 3
-        
+        }
+    @IBAction func redSliderMoved(_ sender: UISlider) {
+        let redColor = String(format: "%.0f", redSlider.value)
+        redLabelValue.text = "\(redColor)"
+
+        let greenColor = String(format: "%.0f", greenSlider.value)
+        greenLabelValue.text = "\(greenColor)"
+
+        let blueColor = String(format: "%.0f", blueSlider.value)
+            blueLabelValue.text = "\(blueColor)"
+
+
     }
     
-    @IBAction func colorViewController(_ sender: UISlider) {
+    @IBAction func changeViewColor(_ sender: UISlider) {
         switch sender.tag {
-        case 1: self.red = redSlider.value;
+        case 1: self.red = redSlider.value
             break;
         case 2: self.green = greenSlider.value
             break;
@@ -52,25 +56,76 @@ class ViewController: UIViewController {
         default:
             break;
         }
+
         self.view.backgroundColor =  UIColor(red: CGFloat( self.red/255.0), green: CGFloat(self.green/255.0), blue: CGFloat(self.blue/255.0), alpha: 1.0)
         
-    }
-    
-    @IBAction func changeSliderValues(_ sender: UISlider) {
-        switch sender.tag {
-        case 1: rdLabelValue.text = "\(redSlider.value.rounded())"
-            break;
-        case 2: grLabelValue.text = "\(greenSlider.value.rounded())"
-            break;
-        case 3: blLabelValue.text = "\(blueSlider.value.rounded())"
-            break;
-        default:
-            break;
-        }
-    }
+        
+}
     
     
 }
+    
+    
+    
+    
+    
+    
 
 
 
+
+
+
+
+
+
+
+////@IBOutlet var colorViewController: UIView!
+//
+//var red: Float = 0
+//var green: Float = 0
+//var blue: Float = 0
+//
+//let roundedValue = {
+//
+//}
+//
+//override func viewDidLoad() {
+//    super.viewDidLoad()
+//    // Do any additional setup after loading the view.
+//    redSlider.tag = 1
+//    greenSlider.tag = 2
+//    blueSlider.tag = 3
+//
+//    rdLabelValue.tag = 1
+//    grLabelValue.tag = 2
+//    blLabelValue.tag = 3
+//
+//}
+//
+//@IBAction func colorViewController(_ sender: UISlider) {
+//    switch sender.tag {
+//    case 1: self.red = redSlider.value;
+//        break;
+//    case 2: self.green = greenSlider.value
+//        break;
+//    case 3: self.blue = blueSlider.value
+//        break;
+//    default:
+//        break;
+//    }
+//    self.view.backgroundColor =  UIColor(red: CGFloat( self.red/255.0), green: CGFloat(self.green/255.0), blue: CGFloat(self.blue/255.0), alpha: 1.0)
+//
+//}
+//
+//@IBAction func changeSliderValues(_ sender: UISlider) {
+//    switch sender.tag {
+//    case 1: rdLabelValue.text = "\(redSlider.value.rounded())"
+//        break;
+//    case 2: grLabelValue.text = "\(greenSlider.value.rounded())"
+//        break;
+//    case 3: blLabelValue.text = "\(blueSlider.value.rounded())"
+//        break;
+//    default:
+//        break;
+//    }
